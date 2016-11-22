86a1150c4aa3570395bfd2f02ff501fc98943730
Harry Trinder <ht16tp@brocku.ca>
2016-11-21 20:10:46 -0500

    WEEK 11 LAB

---
+++ WEEK11/script1.js
@@ -0,0 +1,12 @@
+function text(){
+
+
+
+  var num = document.getElementById("number").value;
+  for (i=0;i<=num;i++){
+    document.getElementById("number2").innerHTML += i+"<br />";
+      console.log('here');
+  }
+
+
+}
